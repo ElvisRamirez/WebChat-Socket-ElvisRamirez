ba0d3396b801e09174c47b6d69ae196635863937
Paulo Galarza <paulo-123@hotmail.es>
2025-05-26 21:11:19 -0500

    Estructura inicial del proyecto

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { createServer } = require('http');
+const realTimeServer = require('./realTimeServer');
+const path = require('path');
+
+const app = express();
+const httpServer = createServer(app);
+
+// Configuraciones
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+
+// Rutas
+app.use(require('./routes'));
+
+// Public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Iniciar el servidor
+httpServer.listen(app.get('port'), () => {
+    console.log(`Servidor corriendo en http://localhost:${app.get('port')}`);
+})
+
+// Llamo al servidor en tiempo real
+realTimeServer(httpServer);
